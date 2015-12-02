b4baf974c42bc3ad9625e7176b34919e60992158
ostrbor <ostrbor@gmail.com>
2015-12-02 15:04:04 +0300

    better organized

--- otchet.py
+++ otchet.py
@@ -1,21 +1,32 @@
 #!/usr/bin/python3
 import pymysql
 from setting import db_info
-from sql import sql_template
+from sql import sql_template, sql_keys
+from datetime import datetime
+import re
 
 
-connection = pymysql.connect(**db_info)
-
-try:
+def make_query(sql):
     with connection.cursor() as cursor:
-        cursor.execute(sql_template)
-        result = cursor.fetchone()
- 	        while result:
-                    print(result)
-                    result = cursor.fetchone()
-        #print(result)
+        cursor.execute(sql)
+        while True:
+            result = cursor.fetchone()
+            if not result: break
+            print(result)
 
-finally:
-    connection.close()
+def get_input():
+    print('Making query to database %s' % db_info['db'])
+    print('Warning! Date format must be YYYY-mm-dd.')
+    date_re = r'\d{4}-\d{2}-\d{2}'
+    msg = 'Please enter %s: '
+    res = {k: input(msg % k) for k in sql_keys}
+    for (k, v) in res:
+        if re.search(date_re, v):
+            res[k] = datetime.strptime(v, '%Y-%m-%d')
+    return res
 
-
+if __name__ == '__main__':
+    user_data = get_input()
+    sql = sql_template % user_data
+    with pymysql.connect(**db_info):
+        make_query(sql)
